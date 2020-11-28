60cc4e92d42553b1cbac58896a59e20bae141145
Binary010100 <75156380+Binary010100@users.noreply.github.com>
2020-11-28 12:07:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+function setup() {
+  createCanvas(400, 400);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(0);
+  translate(200, 200);
+  rotate(-90);
+
+    let hours = hour();
+    let minutes = minute();
+    let seconds = second();
+  
+
+  strokeWeight(8);
+  stroke(255,100,150);
+  noFill();
+  let secondAngle = map(seconds, 0, 60, 0, 360);
+  arc(0, 0, 260, 260, 0, secondAngle);
+
+  stroke(100,255,150);
+  let minuteAngle = map(minutes, 0, 60, 0, 360);
+  arc(0, 0, 280, 280, 0, minuteAngle);
+
+  stroke(100,150,255);
+  let hourAngle = map(hours % 12, 0, 12, 0, 360);
+  arc(0, 0, 300, 300, 0, hourAngle);
+
+  push();
+  rotate(secondAngle);
+  stroke(255, 100, 150);
+  line(0, 0, 100, 0);
+  pop();
+
+  push();
+  rotate(minuteAngle);
+  stroke(150, 255, 100);
+  line(0, 0, 75, 0);
+  pop();
+
+  push();
+  rotate(hourAngle);
+  stroke(150, 100, 255);
+  line(0, 0, 50, 0);
+  pop();
+
+  stroke(255);
+  point(0, 0);
+
+  //  fill(255);
+  //  noStroke();
+  //  text(hr + ':' + mn + ':' + sc, 10, 200);
+}
